210bcddd069a3d1d03621e2d4c5955a00c8f2586
Omar Colombari <omarucolombari@gmail.com>
2025-03-05 15:43:03 -0300

    feat: create use case to edit task

---
+++ src/domain/management/application/use-cases/edit-task.ts
@@ -0,0 +1,44 @@
+import { type Either, left, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import type { Task } from '../../enterprise/entities/task'
+import type { TasksRepository } from '../repositories/tasks-repository'
+import { TaskNotFoundError } from './errors/task-not-found-error'
+
+interface EditTaskUseCaseRequest {
+  name: string
+  description: string
+  taskId: string
+}
+
+type EditTaskUseCaseResponse = Either<
+  TaskNotFoundError,
+  {
+    task: Task
+  }
+>
+
+@Injectable()
+export class EditTaskUseCase {
+  constructor(private tasksRepository: TasksRepository) {}
+
+  async execute({
+    name,
+    taskId,
+    description,
+  }: EditTaskUseCaseRequest): Promise<EditTaskUseCaseResponse> {
+    const task = await this.tasksRepository.findById(taskId)
+
+    if (!task) {
+      return left(new TaskNotFoundError())
+    }
+
+    task.name = name
+    task.description = description
+
+    await this.tasksRepository.save(task)
+
+    return right({
+      task,
+    })
+  }
+}
