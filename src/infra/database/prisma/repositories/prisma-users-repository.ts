e8a8dd83d90e0f419703bdf92db36ea2cdaa2337
Omar Colombari <omarucolombari@gmail.com>
2025-03-05 18:00:54 -0300

    feat: implement prisma users repository

---
+++ src/infra/database/prisma/repositories/prisma-users-repository.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common'
+import type { UsersRepository } from '@/domain/management/application/repositories/users-repository'
+import type { User } from '@/domain/management/enterprise/entities/user'
+import { PrismaUserMapper } from '../mappers/prisma-user-mapper'
+import type { PrismaService } from '../prisma.service'
+
+@Injectable()
+export class PrismaUsersRepository implements UsersRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!user) {
+      return null
+    }
+
+    return PrismaUserMapper.toDomain(user)
+  }
+
+  async create(user: User) {
+    const data = PrismaUserMapper.toPrisma(user)
+
+    await this.prisma.user.create({
+      data,
+    })
+  }
+}
