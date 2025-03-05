471e5773cc995b3be4b8b9fff2fe8ce5b5b293d3
Omar Colombari <omarucolombari@gmail.com>
2025-03-05 16:48:06 -0300

    feat: create use case to authenticate user

---
+++ src/domain/management/application/use-cases/authenticate-user.ts
@@ -0,0 +1,56 @@
+import { type Either, left, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import type { UsersRepository } from '../repositories/users-repository'
+import type { HashComparer } from '../cryptography/hash-comparer'
+import type { Encrypter } from '../cryptography/encrypter'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
+
+interface AuthenticateUserUseCaseRequest {
+  email: string
+  password: string
+}
+
+type AuthenticateUserUseCaseResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string
+  }
+>
+
+@Injectable()
+export class AuthenticateUserUseCase {
+  constructor(
+    private usersRepository: UsersRepository,
+    private hashComparer: HashComparer,
+    private encrypter: Encrypter
+  ) {}
+
+  async execute({
+    email,
+
+    password,
+  }: AuthenticateUserUseCaseRequest): Promise<AuthenticateUserUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      return left(new WrongCredentialsError())
+    }
+
+    const isPasswordValid = await this.hashComparer.compare(
+      password,
+      user.password
+    )
+
+    if (!isPasswordValid) {
+      return left(new WrongCredentialsError())
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: user.id.toString(),
+    })
+
+    return right({
+      accessToken,
+    })
+  }
+}
