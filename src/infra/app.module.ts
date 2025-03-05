eca9af7361560c7f057d50e57ef24824426e2923
Omar Colombari <omarucolombari@gmail.com>
2025-03-05 16:23:44 -0300

    feat: craete env module for validation of environment variables

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env/env'
+import { EnvModule } from './env/env.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validate: env => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    EnvModule,
+  ],
 })
 export class AppModule {}
--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { EnvService } from './env/env.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { cors: true })
+  const envService = app.get(EnvService)
+
+  const port = envService.get('PORT')
+
+  await app.listen(port)
 }
-bootstrap();
+
+bootstrap()
