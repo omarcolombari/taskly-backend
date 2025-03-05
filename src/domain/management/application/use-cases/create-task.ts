6d82d4fb03612903081fb802e9c0b7e5075d0706
Omar Colombari <omarucolombari@gmail.com>
2025-03-05 15:17:27 -0300

    feat: create use case to create task

---
+++ src/domain/management/application/use-cases/create-task.ts
@@ -0,0 +1,42 @@
+import { type Either, left, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import { Task } from '../../enterprise/entities/task'
+import type { HashGenerator } from '../cryptography/hash-generator'
+import type { TasksRepository } from '../repositories/tasks-repository'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+interface CreateTaskUseCaseRequest {
+  name: string
+  description: string
+  userId: string
+}
+
+type CreateTaskUseCaseResponse = Either<
+  null,
+  {
+    task: Task
+  }
+>
+
+@Injectable()
+export class CreateTaskUseCase {
+  constructor(private tasksRepository: TasksRepository) {}
+
+  async execute({
+    name,
+    userId,
+    description,
+  }: CreateTaskUseCaseRequest): Promise<CreateTaskUseCaseResponse> {
+    const task = Task.create({
+      name,
+      userId: new UniqueEntityId(userId),
+      description,
+    })
+
+    await this.tasksRepository.create(task)
+
+    return right({
+      task,
+    })
+  }
+}
