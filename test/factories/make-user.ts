3cea826b462cd010aba3a9ba0fb9baf8adf72918
Omar Colombari <omarucolombari@gmail.com>
2025-03-05 19:02:32 -0300

    test: create e2e test for authenticate controller

--- test/factories/make-user.ts
+++ test/factories/make-user.ts
@@ -5,6 +5,9 @@ import {
   User,
   type UserProps,
 } from '@/domain/management/enterprise/entities/user'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaUserMapper } from '@/infra/database/prisma/mappers/prisma-user-mapper'
 
 export function makeUser(
   override: Partial<UserProps> = {},
@@ -21,4 +24,19 @@ export function makeUser(
   )
 
   return user
+}
+
+@Injectable()
+export class UserFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaUser(data: Partial<UserProps> = {}): Promise<User> {
+    const user = makeUser(data)
+
+    await this.prisma.user.create({
+      data: PrismaUserMapper.toPrisma(user),
+    })
+
+    return user
+  }
 }
