9afa2b18ea05c6565862eefa46f7e91f6ea646a5
Omar Colombari <omarucolombari@gmail.com>
2025-03-06 02:06:01 -0300

    test: create e2e test for create task controller

---
+++ src/infra/http/controllers/create-task.controller.e2e-spec.ts
@@ -0,0 +1,54 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { UserFactory } from 'test/factories/make-user'
+
+describe('Create task (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let userFactory: UserFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [UserFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+    userFactory = moduleRef.get(UserFactory)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[POST] /tasks', async () => {
+    const user = await userFactory.makePrismaUser()
+
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const response = await request(app.getHttpServer())
+      .post('/tasks')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        name: 'New task',
+        description: 'Task test for e2e test.',
+      })
+
+    expect(response.statusCode).toBe(201)
+
+    const taskOnDatabase = await prisma.task.findFirst({
+      where: {
+        name: 'New task',
+      },
+    })
+
+    expect(taskOnDatabase).toBeTruthy()
+  })
+})
